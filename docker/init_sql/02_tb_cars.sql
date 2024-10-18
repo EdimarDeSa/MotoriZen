7727a1354b6dac92c68daef4d4213eca94b9c5c6
Edimar de Sa <edimar.sa@efscode.com.br>
2024-10-18 17:48:05 -0300

    Updating docker configuration

--- docker/init_sql/02_tb_cars.sql
+++ docker/init_sql/02_tb_cars.sql
@@ -1,5 +1,3 @@
-CREATE SCHEMA IF NOT EXISTS motorizen;
-
 SET search_path TO motorizen;
 
 -- Função que previne a modificação manual do campo creation
@@ -44,25 +42,21 @@ END;
 $$ LANGUAGE plpgsql;
 
 
-CREATE TABLE IF NOT EXISTS "tb_user"
+CREATE TABLE IF NOT EXISTS "tb_car"
 (
-    "id_user" UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    "first_name" VARCHAR(50) NOT NULL,
-    "last_name" VARCHAR(100) NOT NULL,
-    "email" VARCHAR(255) NOT NULL UNIQUE,
-    "birthdate" DATE NOT NULL,
-    "cd_auth" UUID NOT NULL,
-    "is_active" BOOLEAN DEFAULT TRUE NOT NULL,
+    "id_car" UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    "cd_user" UUID NOT NULL REFERENCES "tb_user"("id_user") ON DELETE CASCADE,
+    "odometer" FLOAT NOT NULL DEFAULT 0.0,
     "last_update" TIMESTAMP,
     "creation" TIMESTAMP
 );
 
 CREATE TRIGGER prevent_set_user_creation_and_last_update_field_on_insert
-BEFORE INSERT ON "tb_user"
+BEFORE INSERT ON "tb_car"
 FOR EACH ROW
 EXECUTE FUNCTION check_manually_changes_on_timestamp_fields();
 
 CREATE TRIGGER prevent_set_user_creation_and_last_update_field_on_update
-BEFORE UPDATE ON "tb_user"
+BEFORE UPDATE ON "tb_car"
 FOR EACH ROW
 EXECUTE FUNCTION check_manually_changes_on_timestamp_fields();
