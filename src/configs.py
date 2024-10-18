4572a55b624e45137de25766b1757d366aa2e79c
Edimar de Sa <edimar.sa@efscode.com.br>
2024-10-18 17:53:02 -0300

    Adding new router

--- src/configs.py
+++ src/configs.py
@@ -1,4 +1,11 @@
 from dotenv import load_dotenv
+from fastapi import FastAPI
+
+from Enums import MotorizenErrorEnum
+from ErrorHandler import MotorizenError
+from Routers.auth_router import AuthRouter
+from Routers.base_router import BaseRouter
+from Routers.user_router import UserRouter
 
 load_dotenv()
 
@@ -8,6 +15,37 @@ import sys
 from logging import Handler, Logger
 from logging.handlers import RotatingFileHandler
 from pathlib import Path
+
+__all__ = [
+    "register_routers",
+    "CONTACT",
+    "TITLE",
+]
+
+
+### ROUTERS ###
+ROUTERS: list[type[BaseRouter]] = [
+    AuthRouter,
+    UserRouter,
+]
+
+
+def register_routers(app: FastAPI) -> None:
+    for router in ROUTERS:
+        r = router()
+
+        if not isinstance(r, BaseRouter):
+            raise MotorizenError(
+                detail="Invalid router, must be an instance of Routers.base_router.BaseRouter",
+                err=MotorizenErrorEnum.INVALID_ROUTER,
+            )
+
+        logger.debug(f"Starting - {r.__class__.__name__}")
+        app.include_router(r.router)
+
+        for route in r.router.routes:
+            logger.debug(f"Route: {list(route.methods)[0]} - {route.tags[0]} - {route.path}")
+
 
 ### PROJECT INFO ###
 TITLE: str = "MotoriZen – Controle de Ganhos, KM e Consumo para Motoristas"
@@ -34,15 +72,19 @@ if DEBUG_MODE:
     console_handler: Handler = logging.StreamHandler(stream=sys.stdout)
 
     log_file: Path = Path(__file__).resolve().parent / "logs" / "app.log"
-    rotating_handler: RotatingFileHandler = RotatingFileHandler(log_file, maxBytes=2000, backupCount=5)
+    # rotating_handler: RotatingFileHandler = RotatingFileHandler(log_file, maxBytes=2000, backupCount=5)
 
     logging.basicConfig(
         level=logging.DEBUG,
         format=log_format,
         encoding="utf-8",
-        handlers=[console_handler, rotating_handler],
+        handlers=[console_handler],
         datefmt="%Y-%m-%d %H:%M:%S",
     )
 
     logger = logging.getLogger(__name__)
     logger.info("Logger activated!")
+
+
+### VERSION ###
+VERSION: str = "0.0.1"
--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
-from configs import CONTACT, TITLE
+from fastapi import FastAPI
+
+from configs import CONTACT, TITLE, VERSION, register_routers
 from db import DBConnectionHandler
 
 
@@ -9,6 +11,33 @@ def main() -> None:
 
     db_session = DBConnectionHandler.create_session(write=True)
     DBConnectionHandler.test_connection(db_session)
+
+
+app = FastAPI(
+    title=TITLE,
+    description=TITLE,
+    version=VERSION,
+    contact=CONTACT,
+    license_info={"name": "MIT License", "url": "https://opensource.org/licenses/MIT"},
+)
+
+register_routers(app)
+
+
+@app.get("/", summary="Home", tags=["Home"])
+def home() -> str:
+    """
+    Home page
+    """
+    return "MotoriZen API"
+
+
+@app.get("/version", summary="Version", tags=["Utils"])
+def home() -> str:
+    """
+    Home page
+    """
+    return {"version": VERSION}
 
 
 if __name__ == "__main__":
