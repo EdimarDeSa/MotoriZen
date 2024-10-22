6cdf21510cc6f9ee427cb53e552741b7a04eda22
Edimar de Sa <edimar.sa@efscode.com.br>
2024-10-22 00:55:12 -0300

    Creating some new errors

--- src/Enums/motorizen_error_enum.py
+++ src/Enums/motorizen_error_enum.py
@@ -5,8 +5,10 @@ class MotoriZenErrorEnum(IntEnum):
     USER_NOT_ACTIVE = -100
     USER_NOT_FOUND = -104
     USER_ALREADY_EXISTS = -109
+    TOKEN_EXPIRED = -110
 
     LOGIN_ERROR = -200
+    LOGOUT_ERROR = -201
 
     UNKNOWN_ERROR = -999
 
