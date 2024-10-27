6f2f7b25d70f2efaa5dde8887dbef094fb5e66a7
Edimar de Sa <edimar.sa@efscode.com.br>
2024-10-27 01:50:35 -0300

    Creation of new generic type

--- src/Utils/custom_primitive_types.py
+++ src/Utils/custom_primitive_types.py
@@ -1,7 +1,10 @@
-from typing import Any, Literal
+from datetime import date, time
+from typing import Any, Literal, TypeVar
 
 PerPageOptions = Literal[10, 20, 30, 40, 50, 100, 200, 300, 400, 500, 1000]
 
 SortOrderOptions = Literal["asc", "desc"]
 
 Headers = dict[str, Any]
+
+T = TypeVar("T")
