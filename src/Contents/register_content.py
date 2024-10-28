0509a00da91337758bc4cff4d56b056117176bcc
Edimar de Sa <edimar.sa@efscode.com.br>
2024-10-28 15:27:28 -0300

    Changed base_content strategy and register contetnt returns

--- src/Contents/register_content.py
+++ src/Contents/register_content.py
@@ -1,5 +1,7 @@
+from pydantic import Field
+
 from Contents.base_content import BaseContent
-from DB.Models import RegisterModel, RegisterQueryResponseModel
+from DB.Models import CarModel, RegisterModel, RegisterQueryResponseModel
 
 
 class RegisterContent(BaseContent):
@@ -8,3 +10,8 @@ class RegisterContent(BaseContent):
 
 class RegistersContent(BaseContent):
     data: RegisterQueryResponseModel
+
+
+class RegisterNewContent(BaseContent):
+    register_data: RegisterModel = Field(description="Register data inserted.")
+    car_data: CarModel = Field(description="New car data after odometer update.")
