e7a54c04f402cf95fcfeef30a3c0fbbcb71995c6
Edimar de Sa <edimar.sa@efscode.com.br>
2024-11-04 12:48:16 -0300

    Creation os constants

--- src/Utils/constants.py
+++ src/Utils/constants.py
@@ -1,2 +1,27 @@
+from typing import Literal
+
 CAR_LABEL = "id_car"
-REGISTER_DATE = "register_date"
+
+PERIODE_START_DATE = "periode_start_date"
+
+ASCENDANT = ASC = "asc"
+
+DESCENDANT = DESC = "desc"
+
+DAILY: Literal["day"] = "day"
+
+WEEKLY: Literal["week"] = "week"
+
+MONTHLY: Literal["month"] = "month"
+
+YEARLY: Literal["year"] = "year"
+
+ID_CAR = "id_car"
+
+CD_CAR = "cd_car"
+
+RESULT = "result"
+
+INITIAL_DATE = "initial_date"
+
+FINAL_DATE = "final_date"
