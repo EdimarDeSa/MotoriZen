db3518e79c5bd7e0bfa23daa5df7bad0e8a5b563
Edimar de Sa <edimar.sa@efscode.com.br>
2024-11-04 12:36:47 -0300

    Creation of ReportsContent

--- src/Contents/reports_content.py
+++ src/Contents/reports_content.py
@@ -4,9 +4,8 @@ from typing import Any, Literal
 from pydantic import Field
 
 from Contents.base_content import BaseContent
+from DB.Models.report_response_model import ReportResponseModel
 
 
 class ReportsContent(BaseContent):
-    data: dict[uuid.UUID | Literal["-1"], dict[str, Any]] = Field(
-        default_factory=dict, description="Return data, if any"
-    )
+    data: ReportResponseModel = Field(default_factory=dict, description="Return data, if any.")
