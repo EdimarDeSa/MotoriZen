0c04ec7455c9f9b0534d39b24d9c752613fdf5c9
Edimar de Sa <edimar.sa@efscode.com.br>
2024-12-11 15:36:48 -0300

    Updating sql init files

---
+++ docker/init_sql/03_tb_fuel_type.sql
@@ -0,0 +1,15 @@
+SET search_path TO motorizen;
+
+CREATE TABLE IF NOT EXISTS "tb_fuel_type"
+(
+    "id_fuel_type" SERIAL PRIMARY KEY NOT NULL,
+    "name" VARCHAR(10) NOT NULL UNIQUE
+);
+
+
+INSERT INTO "tb_fuel_type" ("name")
+VALUES
+    ('Alcohol'),
+    ('Eletric'),
+    ('Gasoline'),
+    ('Hybrid');
