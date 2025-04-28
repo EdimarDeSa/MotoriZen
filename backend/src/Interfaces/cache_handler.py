75c6be35ea64f5fa5731816cbaa7ee7e7486767b
Edimar de Sa <edimar.sa@efscode.com.br>
2025-04-28 13:26:10 -0300

    Refatoração do handler do redis

---
+++ backend/src/Interfaces/cache_handler.py
@@ -0,0 +1,65 @@
+import logging
+from abc import ABC, abstractmethod
+from enum import IntEnum
+from typing import Any, Optional
+
+from Utils.custom_primitive_types import CacheDataType
+
+
+class CacheHandler(ABC):
+    __logger: logging.Logger | None = None
+
+    @property
+    def _logger(self) -> logging.Logger:
+        return self.__logger
+
+    def create_logger(self, name: str) -> None:
+        self.__logger = logging.getLogger(name)
+
+    __client: object | None = None
+
+    @property
+    def _client(self) -> object:
+        return self.__client
+
+    def _set_client(self, client: object) -> None:
+        self.__client = client
+
+    @abstractmethod
+    def _connect_client(self, db: IntEnum) -> None: ...
+
+    @abstractmethod
+    def _disconnect_client(self) -> None: ...
+
+    def _execute_redis_command(self, db: IntEnum, operation, *args, **kwargs) -> Any | None:
+        """Executa um comando no cache com tratamento de conexão e exceção."""
+        try:
+            self._connect_client(db)
+            return operation(*args, **kwargs)
+        except Exception as e:
+            self._logger.error(f"Erro ao executar comando Redis: {e}")
+            raise
+        finally:
+            self._disconnect_client()
+
+    @abstractmethod
+    def set_data(self, db: IntEnum, key: str, value: CacheDataType, ex: Optional[int] = None) -> Any: ...
+
+    @abstractmethod
+    def set_data_for_user(
+        self,
+        db: IntEnum,
+        user_id: str,
+        hash_key: str,
+        value: CacheDataType,
+        ex: int,
+    ) -> Any: ...
+
+    @abstractmethod
+    def get_data(self, db: IntEnum, key: str) -> CacheDataType: ...
+
+    @abstractmethod
+    def get_data_from_user(self, db: IntEnum, user_id: str, b64_key: str) -> CacheDataType: ...
+
+    @abstractmethod
+    def delete_data(self, db: IntEnum, key: str) -> Any: ...
