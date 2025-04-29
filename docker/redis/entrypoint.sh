e5680d5307aad425636dcadc8f56a5408a625b83
Edimar de Sa <edimar.sa@efscode.com.br>
2025-04-28 23:07:37 -0300

    Ajustada configuração do redis para aumentar segurança e ajustar a quantidade de dbs

--- docker/redis/entrypoint.sh
+++ docker/redis/entrypoint.sh
@@ -4,62 +4,9 @@
 : "${REDIS_PASSWORD:?REDIS_PASSWORD must be set}"
 : "${REDIS_PORT:?REDIS_PORT must be set}"
 
-# Start Redis with the configuration
-cat > /tmp/redis.conf <<EOF
-# Redis Configuration
-always-show-logo yes
-
-# Network
-bind 0.0.0.0
-port $REDIS_PORT
-protected-mode yes
-
-# Security
-user default off
-user ${REDIS_USER} on >${REDIS_PASSWORD} allcommands allkeys
-
-# Memory Management
-maxmemory 256mb
-maxmemory-policy allkeys-lru
-
-# Persistence
-dir /data
-appendonly yes
-appendfilename "appendonly.aof"
-appendfsync everysec
-no-appendfsync-on-rewrite yes
-auto-aof-rewrite-percentage 100
-auto-aof-rewrite-min-size 64mb
-databases 7
-
-# Logging
-loglevel notice
-logfile ""
-
-# Performance
-tcp-keepalive 300
-timeout 0
-tcp-backlog 511
-
-# Snapshotting
-save 900 1
-save 300 10
-save 60 10000
-stop-writes-on-bgsave-error yes
-rdbcompression yes
-rdbchecksum yes
-dbfilename dump.rdb
-
-# Client Connection Settings
-maxclients 20
-
-# Slow Log
-slowlog-log-slower-than 10000
-slowlog-max-len 128
-
-# Latency Monitor
-latency-monitor-threshold 100
-
-EOF
+sed -e "s/{{REDIS_USER}}/${REDIS_USER}/g" \
+    -e "s/{{REDIS_PASSWORD}}/${REDIS_PASSWORD}/g" \
+    -e "s/{{REDIS_PORT}}/${REDIS_PORT}/g" \
+    /usr/local/etc/redis/redis.conf > /tmp/redis.conf
 
 exec redis-server /tmp/redis.conf
