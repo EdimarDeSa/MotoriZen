4268a3433936269b8b7d9869140e8e604483f7a1
Edimar de Sa <edimar.sa@efscode.com.br>
2025-04-30 00:06:20 -0300

    ...

--- backend/src/tests/test_004_vehicles.py
+++ backend/src/tests/test_004_vehicles.py
@@ -7,49 +7,49 @@ from .utils.models import User, Vehicle
 class TestVehiclesSuccess:
     qtd_vehicles_per_user = 5
 
-    # def test_create_vehicle(self, client: TestClient, users: list[User], vehicles: list[Vehicle]) -> None:
-    #     # Given
-    #     offset_index = 0
-    #     for token_data, _ in create_and_athenticate_user(client, users):
-    #         for index in range(self.qtd_vehicles_per_user):
-    #             offset = self.qtd_vehicles_per_user * offset_index
-    #             vehicle = vehicles[index + offset]
+    def test_create_vehicle(self, client: TestClient, users: list[User], vehicles: list[Vehicle]) -> None:
+        # Given
+        offset_index = 0
+        for token_data, _ in create_and_athenticate_user(client, users):
+            for index in range(self.qtd_vehicles_per_user):
+                offset = self.qtd_vehicles_per_user * offset_index
+                vehicle = vehicles[index + offset]
 
-    #             # When
-    #             response = client.post(
-    #                 "/vehicles/new-vehicle",
-    #                 headers={"Authorization": f"Bearer {token_data['access_token']}"},
-    #                 json=vehicle,
-    #             )
+                # When
+                response = client.post(
+                    "/vehicles/new-vehicle",
+                    headers={"Authorization": f"Bearer {token_data['access_token']}"},
+                    json=vehicle,
+                )
 
-    #             # Then
-    #             assert response.status_code == 201
-    #             assert response.json()["rc"] == 0
-    #             assert response.json()["data"]["license_plate"] == vehicle["license_plate"]
+                # Then
+                assert response.status_code == 201
+                assert response.json()["rc"] == 0
+                assert response.json()["data"]["license_plate"] == vehicle["license_plate"]
 
-    #         offset_index += 1
+            offset_index += 1
 
-    # def test_get_all_vehicles(self, client: TestClient, users: list[User], vehicles: list[Vehicle]) -> None:
-    #     # Given
-    #     for _, token_data, _ in create_vehicles(client, users, vehicles, self.qtd_vehicles_per_user):
-    #         # When
-    #         response = client.post(
-    #             "/vehicles/get-vehicles",
-    #             headers={"Authorization": f"Bearer {token_data['access_token']}"},
-    #             json={
-    #                 "query_filters": {},
-    #                 "query_options": {"per_page": 10},
-    #             },
-    #         )
+    def test_get_all_vehicles(self, client: TestClient, users: list[User], vehicles: list[Vehicle]) -> None:
+        # Given
+        for _, token_data, _ in create_vehicles(client, users, vehicles, self.qtd_vehicles_per_user):
+            # When
+            response = client.post(
+                "/vehicles/get-vehicles",
+                headers={"Authorization": f"Bearer {token_data['access_token']}"},
+                json={
+                    "query_filters": {},
+                    "query_options": {"per_page": 10},
+                },
+            )
 
-    #         data = response.json()
-    #         results = data["data"]["results"]
-    #         rc = data["rc"]
+            data = response.json()
+            results = data["data"]["results"]
+            rc = data["rc"]
 
-    #         # Then
-    #         assert response.status_code == 200
-    #         assert rc == 0
-    #         assert len(results) == self.qtd_vehicles_per_user
+            # Then
+            assert response.status_code == 200
+            assert rc == 0
+            assert len(results) == self.qtd_vehicles_per_user
 
     def test_get_specific_vehicle(self, client: TestClient, users: list[User], vehicles: list[Vehicle]) -> None:
         # Given
